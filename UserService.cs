378d3fb1fd1526ce3dd77009c36d653f5bbf4d09
Sudhakar Boina <sudhakarb@proarch.com>
2025-06-12 01:34:59 +0530

    feat: SB-100 added userservice

---
+++ UserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeRabit_Test
+{
+    public class UserService
+    {
+        public void CreateUser(string username, string email)
+        {
+            // Directly using without null/empty validation
+            Console.WriteLine($"Creating user {username.ToUpper()} with email {email.ToLower()}");
+        }
+    }
+
+}
